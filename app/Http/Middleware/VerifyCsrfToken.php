7535a45a3c18ff440383bab124e8bf9b93ff8c68
josephtorresc <j.torrescerbellon@gmail.com>
2022-06-13 18:11:42 -0400

    Add url verify csrf post

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        'http://127.0.0.1:8000/usuarios',
         'http://127.0.0.1:8000/usuarios/*'
     ];
 }
